907277dc32d8d12fefd4b53acef2668d2d9fc44a
anstaks <anstaks@yandex.ru>
2013-08-08 13:17:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ sudo apt-get -y install libmysql++-dev
 if ! type ruby > /dev/null; then
 	curl -L https://get.rvm.io | bash -s stable --ruby
 fi
-source /usr/local/rvm/scripts/rvm
 
+# Install Bundler
 if ! type bundle > /dev/null; then
 	gem install bundler
 fi
